597d1bc058a66902b572f7dbc6ff78809384a095
A1doranM <vaselisk1997@gmail.com>
2020-10-20 15:05:50 +0300

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const sass = require("gulp-sass");
+const rename = require("gulp-rename");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+
+gulp.task("browser-sync", function () {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+});
+
+gulp.task("styles", function () {
+    return gulp.src("src/css/*.+(sass|scss)")
+        .pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
+        .pipe(rename({
+            prefix: "",
+            suffix: ".min",
+        }))
+        .pipe(autoprefixer({
+            browsers: ["last 2 versions"],
+            cascade: false
+        }))
+        .pipe(cleanCSS({compatibility: "ie8"}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task("watch", function () {
+    gulp.watch("src/css/*.+(sass|scss)", gulp.parallel("styles"));
+    gulp.watch("src/*.html").on("change", browserSync.reload);
+});
+
+gulp.task("default", gulp.parallel("watch", "browser-sync", "styles"));
